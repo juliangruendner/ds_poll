aeca48d85d87706895477dbd8a60926d4c73cee7
Julian Gründner <juliangruendner@googlemail.com>
2018-08-21 13:52:48 +0200

    add start option for docker env

--- start.sh
+++ start.sh
@@ -1,2 +1,3 @@
 # python ds_poll.py  # https disabled
-python3 ds_poll.py -o localhost:8843 -s -v #https enabled
+# python3 ds_poll.py -o localhost:8843 -s -v #https enabled
+python3 ds_poll.py -q queue_server:8001 -o datashield_opal:8443 -s -v # start in dockerd
